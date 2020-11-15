e3a8b95217c0d87110aeca6b88aaf24f0ca8f26b
Oleksandr Ivashchuk <67271891+Lookout84@users.noreply.github.com>
2020-11-15 13:30:49 +0200

    7.2

--- js/task-7_1.js
+++ js/task-7_1.js
@@ -1,21 +1,9 @@
 const categoriesRef = document.querySelectorAll('#categories');
-console.log(categoriesRef);
-
+const tittleRef = document.querySelectorAll('h2');
+const itemRef = document.querySelectorAll('li.item')
 const howItem = document.querySelectorAll('li.item').length;
-//const howItem = itemRef.reduce(acc, item)
+
 console.log(`У списку ${howItem} категорії.`);
 
-categoriesRef.forEach((elem) => {console.log(elem.textContent)});
-
-
-const tittleRef = document.querySelectorAll('h2');
-
-const getNameCategories = tittleRef.forEach((elem) => {return elem.textContent});
-
-const itemRef = document.querySelectorAll('#categories ul');
-
-console.log(itemRef);
-
-const getHowElements = itemRef.forEach((elem) => {elem.children.length});
-
-console.log(`Категорія: ${getNameCategories}\nКількість елементів: ${getHowElements}`)
+tittleRef.forEach((elem) => {console.log(`Категорія:`,elem.textContent,
+`\nКількість елементів:`,elem.nextElementSibling.children.length)});
