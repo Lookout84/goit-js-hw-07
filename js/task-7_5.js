3ed199a1e89e4f60e5419edb09cbd20e4c92fe44
Oleksandr Ivashchuk <thelookout84@gmail.com>
2020-11-17 17:28:29 +0200

    Update task-7_5.js

--- js/task-7_5.js
+++ js/task-7_5.js
@@ -4,5 +4,5 @@ const spanRef = document.getElementById('name-output');
 inputRef.addEventListener('input', event => {
 if ( event.target.value == "") {
     spanRef.textContent = 'незнайомець';
-} else spanRef.textContent = event.target.value;
+} else spanRef.textContent = event.target.value.trim();
 });
