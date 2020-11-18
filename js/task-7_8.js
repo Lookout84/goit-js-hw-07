7f17d8dcc16e6de53fe51dbd04a40aaebbbdecf9
Oleksandr Ivashchuk <thelookout84@gmail.com>
2020-11-18 17:19:11 +0200

    Update task-7_8.js

--- js/task-7_8.js
+++ js/task-7_8.js
@@ -1,28 +1,37 @@
 const divControlsRef = document.getElementById('controls');
 const divBoxes = document.getElementById('boxes');
-const inputRef = document.querySelector('#controls input[type="number"]')
-const btnRender = document.querySelector('#controls button[data-action="render"]');
+const inputRef = document.querySelectorAll('#controls input[type="number"]')
+const btnRender = document.querySelectorAll('#controls button[data-action="render"]');
+
+btnRender.value = Number(inputRef.value);
+
+btnRender.addEventListener('change', createBoxes);
 
 
-btnRender.addEventListener('input', createBoxes);
+
+function getValue(){
+    console.log(inputRef.value);
+}
 
 function createBoxes(amount) {
-    //amount = inputRef.value;
-    let x = 30;
-    let y = 30;
+    btnRender.value = Number(inputRef.value);
+    amount = btnRender.value;
+    let x = 20;
+    let r = Math.floor(Math.random() * (256));
+    let g = Math.floor(Math.random() * (256));
+    let b = Math.floor(Math.random() * (256));
     for ( let i = 1; i <= amount; i++) {
     x += 10;
-    y += 10;
     const div = document.createElement('div');
     div.style.height = x + 'px';
-    div.style.width = y + 'px';
-    div.style.margin = x + 'px';
-    div.style.backgroundColor = 'red';
+    div.style.width = x + 'px';
+    div.style.margin = '10px';
+    div.style.padding = 0;
+    div.style.backgroundColor = `rgb(${r / i}, ${g / i}, ${b / i})`;
     divBoxes.append(div);
     }
 }
 
-console.log(inputRef.value);
-console.dir(btnRender)
-
-createBoxes(5)
+console.dir(inputRef);
+console.dir(btnRender);
+//console.log(amount)
